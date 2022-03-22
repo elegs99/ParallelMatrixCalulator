d9a2e3dde9d8a84266414dd180347b3cbf033840
Grant Linke <GLinke123@gmail.com>
2022-03-21 20:07:21 -0400

    Changes formatting
    VSCode did this automatically, feel free to revert this if you find it vomit-inducing

--- matrixmult.java
+++ matrixmult.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-class matrixmult{
+class matrixmult {
     public static int rowA;
     public static int colA;
     public static int rowB;
@@ -14,19 +14,17 @@ class matrixmult{
     public static int[][] matA;
     public static int[][] matB;
     public static int[][] matC;
-    public static AtomicInteger row= new AtomicInteger(0);
+    public static AtomicInteger row = new AtomicInteger(0);
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while(true)
-        {
+        while (true) {
             System.out.println("\nEnter number for operation\n3: Multiply\n5: Exit");
 
             int choice = sc.nextInt();
 
-            if(choice!=5)
-            {
+            if (choice != 5) {
                 System.out.println("Dimensions of matrix A:");
                 System.out.print("Row = ");
                 rowA = sc.nextInt();
@@ -45,38 +43,28 @@ class matrixmult{
 
                 System.out.println("Enter matrix A:");
 
-                for (int i = 0; i < rowA; i++) 
-                {
-                    for (int j = 0; j < colA; j++) 
-                    {
+                for (int i = 0; i < rowA; i++) {
+                    for (int j = 0; j < colA; j++) {
                         matA[i][j] = sc.nextInt();
                     }
                 }
 
                 System.out.println("Enter matrix B:");
 
-                for (int i = 0; i < rowB; i++) 
-                {
-                    for (int j = 0; j < colB; j++) 
-                    {
+                for (int i = 0; i < rowB; i++) {
+                    for (int j = 0; j < colB; j++) {
                         matB[i][j] = sc.nextInt();
                     }
                 }
 
-                if(choice==3)
-                {
-                    if(colA!=rowB)
-                    {
+                if (choice == 3) {
+                    if (colA != rowB) {
                         System.out.println("Matrix multiplication not possible");
-                    }
-                    else
-                    {
-                        multiply(matA,matB,matC);
+                    } else {
+                        multiply(matA, matB, matC);
                     }
                 }
-            }
-            else
-            {
+            } else {
                 break;
             }
         }
@@ -84,12 +72,11 @@ class matrixmult{
         sc.close();
     }
 
-    public static void multiply(int[][] matA, int[][] matB, int[][] matC)
-    {
-        //implementation 1 as discussed in meeting
-        Runnable runnable = ()->{
-            int i = row.get(); //i denotes row number of resultant matC
-    
+    public static void multiply(int[][] matA, int[][] matB, int[][] matC) {
+        // implementation 1 as discussed in meeting
+        Runnable runnable = () -> {
+            int i = row.get(); // i denotes row number of resultant matC
+
             for (int j = 0; j < colB; j++)
                 for (int k = 0; k < rowB; k++)
                     matC[i][j] += matA[i][k] * matB[k][j];
@@ -101,20 +88,15 @@ class matrixmult{
 
         long start = System.nanoTime();
 
-        for(int i=0;i<rowA;i++)
-        {
+        for (int i = 0; i < rowA; i++) {
             threadList.add(new Thread(runnable));
             threadList.get(i).start();
         }
 
-        for(Thread t : threadList)
-        {
-            try
-            {
+        for (Thread t : threadList) {
+            try {
                 t.join();
-            }
-            catch(InterruptedException e)
-            {
+            } catch (InterruptedException e) {
                 return;
             }
         }
@@ -122,16 +104,15 @@ class matrixmult{
         long end = System.nanoTime();
 
         System.out.println("\nResultant matrix:");
-        for (int r = 0; r < rowA; r++) 
-        {
-            for (int c = 0; c < colB; c++) 
-            {
+        for (int r = 0; r < rowA; r++) {
+            for (int c = 0; c < colB; c++) {
                 System.out.print(matC[r][c]);
                 System.out.print(" ");
             }
             System.out.println();
         }
 
-        System.out.println("Executed in "+String.valueOf((end-start)/1000000)+" milliseconds");
+        System.out.println("Executed in " + String.valueOf((end - start) / 1000000) + " milliseconds");
     }
+
 }
